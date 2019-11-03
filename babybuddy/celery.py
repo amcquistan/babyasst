bffadd2547c469c88aaf048bf7b1e8cbdb1a1b69
adam <sci.guy.mcq@gmail.com>
2019-11-03 23:07:02 +0000

    updates

--- babybuddy/celery.py
+++ babybuddy/celery.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'babybuddy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'babybuddy.settings.production')
 
 celery_app = Celery('babybuddy')
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
